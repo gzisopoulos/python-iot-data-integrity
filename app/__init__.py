21d05d8daeef5eb7b94f70df9063f8b8a4ab45ce
geroge zisopoulos <zisop@veturilo.io>
2020-07-06 19:18:10 +0300

    implement Init file

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+
+def create_app():
+    app = Flask(__name__)
+    dotenv_path = os.path.join(os.path.dirname(app.root_path), 'config/.env')
+    load_dotenv(dotenv_path)
+    app.config.from_object('config.Config')
+    return app
+
+
+app = create_app()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+alfred_logger = logging.getLogger('alfred_logger')
+app.logger.handlers.extend(alfred_logger.handlers)
+
+from app.routes import routes
