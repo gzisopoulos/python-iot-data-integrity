0cd4f62d87920592b7fdef0fa1e1ce8352fd7f68
geroge zisopoulos <zisop@veturilo.io>
2020-07-08 12:55:20 +0300

    Update comments on Init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,22 @@
 import logging
-import os
 
 from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 
 
 def create_app():
     app = Flask(__name__)
-    dotenv_path = os.path.join(os.path.dirname(app.root_path), 'config/.env')
-    load_dotenv(dotenv_path)
     app.config.from_object('config.Config')
     return app
 
 
 app = create_app()
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-alfred_logger = logging.getLogger('alfred_logger')
-app.logger.handlers.extend(alfred_logger.handlers)
+dt_logger = logging.getLogger('dt_logger')
+app.logger.handlers.extend(dt_logger.handlers)
 logging.getLogger().setLevel(logging.INFO)
 
-
+# There are different ways to create an init file.
+# From our point of view this is one of the simplest
 from app.routes import routes
