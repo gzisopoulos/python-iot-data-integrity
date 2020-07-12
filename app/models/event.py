1860f64a408bfdb3327abe6e89745d1ac881062f
geroge zisopoulos <zisop@veturilo.io>
2020-07-12 17:07:52 +0300

    Update event model

--- app/models/event.py
+++ app/models/event.py
@@ -1,10 +1,20 @@
 from sqlalchemy import func
 
-from app import db
+from app import app, db
 from app.models.base import BaseModel
 
 
 class Event(BaseModel):
+    """
+    All device signals and fields are stored inside this model.
+    In real-world example this will be the most crucial table of your database.
+    We prefer to include it in its own database.
+
+    Note that our fields are verbose in order to understand them. It is a bad
+    practice to keep them that way.
+    """
+    __bind_key__ = 'ingest_db'
+
     device_number = db.Column(db.String, index=True)
     event_code = db.Column(db.String, index=True)
     message_date = db.Column(db.DateTime, index=True)
@@ -25,7 +35,8 @@ class Event(BaseModel):
         """
         Returns a list of randomized events.
         """
-        randomized_set = cls.query.order_by(func.random()).limit(int(count) / 200).all()
+        randomized_set = cls.query.order_by(func.random()).limit(
+            int(count) / int(app.config['RANDOMIZED_SET_SCALE'])).all()
         return randomized_set
 
     @classmethod
