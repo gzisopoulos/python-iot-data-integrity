5af121a31532094d9d54af7f676eed51071875b3
Theofanis Petkos <tpetkos@veturilo.io>
2020-07-13 10:58:16 +0300

    Apply list of services to hartbeat task

--- app/tasks/check_heartbeat.py
+++ app/tasks/check_heartbeat.py
@@ -4,6 +4,8 @@ import datetime
 from app import app
 from app.models.service import Service
 from app.tasks.task_utils import bind_to_service
+from app.routes.routes import services_list
+from app.tasks.build_tasks.create_services import create_services
 
 
 async def check_heartbeat(timeout):
@@ -11,18 +13,22 @@ async def check_heartbeat(timeout):
     Fetches all available services and binds to their ports in order to check if their are up.
     """
     while True:
+        # Periodic task with infinite loop and timeout
         await asyncio.sleep(int(timeout))
+        # These datetimes are used only for logging
         start = datetime.datetime.now()
-        # TODO - Remove after europython
         app.logger.info('Heartbeat Task started at: %s' % (str(start.strftime('%Y-%m-%d %H:%M:%S'))))
-        services = Service.query.all()
-        if services:
+        if services_list:
+            # build a response for hartbeat task
             response = ''
-            for service in services:
-                running = bind_to_service(service.ip_address, service.port)
+            for service in services_list:
+                running = bind_to_service(service['ip_address'], service['port'])
+                # if one service is down build the response with every not running service name
+                # this is a simple log notification. You may want to send an email to your admin
                 if not running:
-                    response = response + service.name + ' '
+                    response = response + service['name'] + ' '
             if response:
+                # Total time is only for logging purposes
                 total_time = int((datetime.datetime.now() - start).microseconds / 1000)
                 app.logger.info('Finished in %s ms. DOWN: %s' % (str(total_time), response))
             else:
@@ -31,4 +37,5 @@ async def check_heartbeat(timeout):
         else:
             # If no service found iterate
             app.logger.info('No Services found. Re-fetching...')
+            services_list = create_services()
         # Sleep until next iteration.
