92dede2b203eb6d5a8146c4a4ad7d2061a7f6c84
Theofanis Petkos <tpetkos@veturilo.io>
2020-07-13 15:07:16 +0300

    Remove duplicate trace outside of utils

--- app/tasks/task_utils.py
+++ app/tasks/task_utils.py
@@ -4,32 +4,10 @@ import socket
 from app.models.error_trace import ErrorTrace
 
 
-def duplicate_trace(decoded_event):
-    """
-    Checks if the trace already exists.
-    """
-    error_trace = ErrorTrace.query.filter(ErrorTrace.device_number == decoded_event.device_number).filter(
-        ErrorTrace.event_code == decoded_event.event_code).filter(
-        ErrorTrace.message_date == decoded_event.message_date).first()
-    if error_trace:
-        return True
-    else:
-        return False
-
-
-def notify_for_device_check(device_number, error_code, limit):
-    """
-    Checks if this devices has reached the limit of times for specific error_code
-    """
-    error_traces = ErrorTrace.query.filter(ErrorTrace.device_number == device_number).filter(
-        ErrorTrace.error_code == error_code).all()
-    if len(error_traces) >= limit:
-        return True
-    else:
-        return False
-
-
 def bind_to_service(ip_address, port):
+    """
+    Bind to specific address. If it is in use then service is up
+    """
     address = (ip_address, port)
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
