c296f839a915e017839f662f097299a7898ad029
Theofanis Petkos <tpetkos@veturilo.io>
2020-07-13 15:06:28 +0300

    Refactor heartbeat with dict

--- app/tasks/check_heartbeat.py
+++ app/tasks/check_heartbeat.py
@@ -4,11 +4,10 @@ import datetime
 from app import app
 from app.models.service import Service
 from app.tasks.task_utils import bind_to_service
-from app.routes.routes import services_list
 from app.tasks.build_tasks.create_services import create_services
 
 
-async def check_heartbeat(timeout):
+async def check_heartbeat(services_dict, timeout):
     """
     Fetches all available services and binds to their ports in order to check if their are up.
     """
@@ -18,15 +17,15 @@ async def check_heartbeat(timeout):
         # These datetimes are used only for logging
         start = datetime.datetime.now()
         app.logger.info('Heartbeat Task started at: %s' % (str(start.strftime('%Y-%m-%d %H:%M:%S'))))
-        if services_list:
+        if services_dict:
             # build a response for hartbeat task
             response = ''
-            for service in services_list:
-                running = bind_to_service(service['ip_address'], service['port'])
+            for service_name in services_dict.keys():
+                running = bind_to_service(services_dict[service_name]['ip_address'], services_dict[service_name]['port'])
                 # if one service is down build the response with every not running service name
                 # this is a simple log notification. You may want to send an email to your admin
                 if not running:
-                    response = response + service['name'] + ' '
+                    response = response + service_name + ' '
             if response:
                 # Total time is only for logging purposes
                 total_time = int((datetime.datetime.now() - start).microseconds / 1000)
@@ -37,5 +36,5 @@ async def check_heartbeat(timeout):
         else:
             # If no service found iterate
             app.logger.info('No Services found. Re-fetching...')
-            services_list = create_services()
+            services_dict = create_services()
         # Sleep until next iteration.
