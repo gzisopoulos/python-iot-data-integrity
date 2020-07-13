1ec1667e2ca456e838bfd67b3b21a90b5e3f6f19
Theofanis Petkos <tpetkos@veturilo.io>
2020-07-13 10:57:13 +0300

    Refactor create services to return a list

--- app/tasks/build_tasks/create_services.py
+++ app/tasks/build_tasks/create_services.py
@@ -4,37 +4,26 @@ from app.models.service import Service
 
 def create_services():
     """
-    Creates all given services on service table.
+    Gathers all services info inside a dict.
     """
-    # Ingest service record
-    ingest_service = Service.find_by_name('Ingest')
-    if not ingest_service:
-        ingest_service = Service(name='Ingest', ip_address=app.config['INGEST_HOST'],
-                                 port=app.config['INGEST_PORT'])
-        try:
-            ingest_service.save_to_db()
-        except Exception as e:
-            db.session.rollback()
-            return {'status': False, 'response': str(e)}
+    # We prefer our integrity module to be independent from db
+    # Here you may use redis or something like this as storage solution
+    services_list = [
+        {
+            'name': 'ingest',
+            'ip_address': app.config['INGEST_HOST'],
+            'port': int(app.config['INGEST_PORT'])
+        },
+        {
+            'name': 'postgresql',
+            'ip_address': app.config['POSTGRESQL_HOST'],
+            'port': int(app.config['POSTGRESQL_PORT'])
+        },
+        {
+            'name': 'rabbitmq',
+            'ip_address': app.config['RMQ_HOST'],
+            'port', int(app.config['RMQ_PORT'])
+        }
+    ]
 
-    # PostgeSQL service record
-    psql_service = Service.find_by_name('PostgreSQL')
-    if not psql_service:
-        psql_service = Service(name='PostgreSQL', ip_address='127.0.0.1', port=5432)
-        try:
-            psql_service.save_to_db()
-        except Exception as e:
-            db.session.rollback()
-            return {'status': False, 'response': str(e)}
-
-    # RabbitMQ service record
-    rabbit_service = Service.find_by_name('RabbitMQ')
-    if not rabbit_service:
-        rabbit_service = Service(name='RabbitMQ', ip_address='127.0.0.1', port=5009)
-        try:
-            rabbit_service.save_to_db()
-        except Exception as e:
-            db.session.rollback()
-            return {'status': False, 'response': str(e)}
-
-    return {'status': True, 'response': 'Services stored'}
+    return services_list
